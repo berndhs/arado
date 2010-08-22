a344e0358d0d9094c112f88c34386ba9e280da65
wall_e <thomasasta@googlemail.com>
2010-08-22 20:24:09 +0000

    de translation

--- translate/arado_de.ts
+++ translate/arado_de.ts
@@ -6,143 +6,143 @@
     <message>
         <location filename="../ui/aradomain.ui" line="14"/>
         <source>MainWindow</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Hauptfenster</translation>
     </message>
     <message>
         <location filename="../ui/aradomain.ui" line="27"/>
         <source>Add URL: Press the logo and add the entered URL to your local database. A pop-up window asks you to add a short URL description or keywords seperated by blanks.</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">URL Hinzufügen: Drücke das Logo und füge die eingegebene URL zu Deiner lokalen Datenbank hinzu. Ein Fenster fragt nach dem Hinzufügen einerkurzen Beschreibung or von Stichworten, getrennt durch die Leertaste.</translation>
     </message>
     <message>
         <location filename="../ui/aradomain.ui" line="53"/>
         <source>Filter your local URL-database for the entered keyword(s).</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Filtere Deine lokale URL-Datenbank nach den eingegebenen Suchworten.</translation>
     </message>
     <message>
         <location filename="../ui/aradomain.ui" line="56"/>
         <source>Search</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Suche</translation>
     </message>
     <message>
         <location filename="../ui/aradomain.ui" line="69"/>
         <source>Show the latest added URLs on top.</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Zeige die neuesten hinzugefügten URLs in einer absteigenden Liste.</translation>
     </message>
     <message>
         <location filename="../ui/aradomain.ui" line="72"/>
         <source>Show Recent</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Zeige aktuelle</translation>
     </message>
     <message>
         <location filename="../ui/aradomain.ui" line="118"/>
         <source>Enter a keyword for a search in your local URL-database or enter any URL to add it to your database.</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Gebe ein Stichwort für die Suche in Deiner lokalen URL-Datenbank ein oder füge eine URL zu Deiner Datenbank hinzu.</translation>
     </message>
     <message>
         <location filename="../ui/aradomain.ui" line="138"/>
         <source>ARADO is a networked URL Database, which saves your added URLs and syncs newest URLs with your connected devices.</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">ARADO ist eine vernetzte URL-Datenbank, welche Deine hinzugefügten URLs speichert und die neuesten URLs mit Deinen verbundenen Geräten synchronisiert.</translation>
     </message>
     <message>
         <location filename="../ui/aradomain.ui" line="141"/>
         <source>ARADO - URL Database</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">ARADO - URL Datenbank</translation>
     </message>
     <message>
         <location filename="../ui/aradomain.ui" line="223"/>
         <source>Enter any online  IP-Adress of your network, if the IP-cache is empty.</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Gebe eine online IP-Adresse von Deinem Netzwerk ein, wenn der IP-Cache leer ist.</translation>
     </message>
     <message>
         <location filename="../ui/aradomain.ui" line="226"/>
         <source>999.999.999.999; </source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">999.999.999.999:</translation>
     </message>
     <message>
         <location filename="../ui/aradomain.ui" line="312"/>
         <source>Add the referring port of the entered IP.</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Füge den entsprechenden Port von der eingegebenen IP-Adresse hinzu.</translation>
     </message>
     <message>
         <location filename="../ui/aradomain.ui" line="334"/>
         <source>Boot with an IP+Port, if the IP-cache is empty or outdated.</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Boote mit einer IP-Adresse und Port, wenn der IP-Zwischenspeicher leer oder nicht mehr aktuell ist.</translation>
     </message>
     <message>
         <location filename="../ui/aradomain.ui" line="337"/>
         <source>Boot</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Boote</translation>
     </message>
     <message>
         <location filename="../ui/aradomain.ui" line="350"/>
         <source>Shows how many incoming and outgoing connections are established (maximum = 30). If connections = 0, please add one online IP+Port and press the boot button to connect and fill the IP-cache.</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Zeigt, wie viele eingehende und ausgehende Verbindungen bestehen (maximum = 30). Wenn die Verbindungen = 0 sind, füge bitte eine online IP-Adresse mit Port hinzu und drücke den Boot-Knopf, um zu verbinden und den IP-Cache zu füllen.</translation>
     </message>
     <message>
         <location filename="../ui/aradomain.ui" line="353"/>
         <source>Connections (in/out): 30/30</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Verbindungen (ein/aus): 30/30</translation>
     </message>
     <message>
         <location filename="../ui/aradomain.ui" line="366"/>
         <source>Amount of IP-adresses in your IP-cache (1500-5000). Rise up, if all IP-adresses are outdated or offline and connections fail after a start.  Then each minute a request is sent to the boot-IP-connections, to fill the cache for 100%.</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Anzahl an IP-Adressen in Deinem IP-Zwischenspeicher (1500-5000). Erhöhe diesen, wenn allle IP-Adressen alt oder offline sind und Verbindungen nach dem Start nicht zustande kommen. Dann wird jede Minute eine Anfrage an die bestehenden Boot-IP-Verbindungen gestellt, um den Zwischenspeicher zu 100% zu füllen.</translation>
     </message>
     <message>
         <location filename="../ui/aradomain.ui" line="388"/>
         <source>IP-Cache Size:</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Größe des IP-Zwischenspeichers</translation>
     </message>
     <message>
         <location filename="../ui/aradomain.ui" line="401"/>
         <source>Open the marked URL in the browser.</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Öffne die markierte URL im Browser.</translation>
     </message>
     <message>
         <location filename="../ui/aradomain.ui" line="404"/>
         <source>Browse URL</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">URL ansehen</translation>
     </message>
     <message>
         <location filename="../ui/aradomain.ui" line="417"/>
         <source>My Port:</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Mein Port:</translation>
     </message>
     <message>
         <location filename="../ui/aradomain.ui" line="430"/>
         <source>You need to forward this port in your NAT/router, if you want to connect.</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Du musst diesen Port im NAT/Router weiterleiten, wenn Du verbinden willst.</translation>
     </message>
     <message>
         <location filename="../ui/aradomain.ui" line="449"/>
         <source>An URLs is transferred each X seconds to each connection. Delay defines the used bandwidth. 0 means as fast as possible.</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Eine URL wird alle X Sekunden transferriert zu jeder Verbindung. Verzögerung definiert die benötigte Bandbreite. 0 meint so schnell als möglich.</translation>
     </message>
     <message>
         <location filename="../ui/aradomain.ui" line="462"/>
         <source>URL-Delay (Sec):</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">URL-Verzögerung (Sek):</translation>
     </message>
     <message>
         <location filename="../ui/aradomain.ui" line="475"/>
         <source>IP-connections of your devices are dropped for sync-balancing reasons each X minutes (10-30).</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">IP-Verbindungen von Deinen Geräten werden abgestossen nach X Minuten (10-30), um die Synkronisation zu balancieren.</translation>
     </message>
     <message>
         <location filename="../ui/aradomain.ui" line="497"/>
         <source>IP-Drop-Time (Min):</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">IP-Abstoßungs-Zeit (Min):</translation>
     </message>
     <message>
         <location filename="../ui/aradomain.ui" line="512"/>
         <location filename="../ui/aradomain.ui" line="523"/>
         <source>Exit</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Beenden</translation>
     </message>
     <message>
         <location filename="../ui/aradomain.ui" line="528"/>
         <source>About Arado</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Über ARADO</translation>
     </message>
 </context>
 <context>
