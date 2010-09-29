f4d1a1b591d623a45fc3bf4dc6245dc0600479ec
Bernd Stramm <berndhs@users.noreply.github.com>
2010-09-29 23:02:31 +0000

    Merge branch 'master' of ssh://pirx.berndnet/gitrepos/arado
    Conflicts:
    	trunk/src/arado-stream-parser.cpp
    	trunk/src/http-client.cpp
    	trunk/src/http-sender.cpp

--- src/http-sender.cpp
+++ src/http-sender.cpp
@@ -203,8 +203,6 @@ HttpSender::ReplyInvalid (const QString & message, int error)
   lines << "\r\n";
   lines << message;
   lines << "\r\n";
-  lines << message;
-  lines << "\r\n";
   lines << QDateTime::currentDateTime().toString () << "\n";
   qDebug () << " sending error message " << lines;
   ostream << lines.join ("");
