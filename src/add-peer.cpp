6e9495f8b260f6dd5b565fd49d364ba8a1d8f95f
wall_e <thomasasta@googlemail.com>
2010-10-07 19:45:26 +0000

    http://www.youtube.com/watch?v=BMkjoQ6S7oQ

--- src/add-peer.cpp
+++ src/add-peer.cpp
@@ -53,7 +53,7 @@ void
 AddPeerDialog::Run ()
 {
   quint64 now = QDateTime::currentDateTime().toTime_t();
-  QString defaultName (tr("local%1").arg(now));
+  QString defaultName (tr("Mr.Smith%1").arg(now));
   addrUi.nickEdit->setText (defaultName);
   addrUi.addressEdit->clear ();
   addrUi.portEdit->clear ();
