d9545ea0e6505df6a59df5c2fb3fa958d6d70656
wall_e <thomasasta@googlemail.com>
2010-10-22 19:45:33 +0000

    update.

--- query.php
+++ query.php
@@ -3,11 +3,12 @@
    arado/query.php 
    so that it can be reached as
    http://your.server.net/arado/query.php
+   the readfile - next to it in the same path - is nameed: addresses.xml
 */
  
 header ("Content-Type: text/xml");
 header ("Connection: close");
 header ("X-Data-Type: ADDR");
-readfile ("addr-list.xml");
+readfile ("addresses.xml");
 echo "\r\n";
 ?>
