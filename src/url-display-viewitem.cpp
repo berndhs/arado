cead5e89c9ff4c4788f43e912644d03357360bd9
wall_e <thomasasta@googlemail.com>
2011-01-14 21:22:02 +0000

    some more cosmetics for this evening, is line 52:  "</div>" in url-display-viewitem.cpp not redundant? dunno, it works.

--- src/url-display-viewitem.cpp
+++ src/url-display-viewitem.cpp
@@ -25,43 +25,51 @@ UrlDisplayViewItem::Html()
                   "<img src=\"qrc:/html/html/images/kugar.png\">"
                   "</a></div>"
              "<h4>"
-             "<a href=\"%1\">%2</a></h4>\n"
+             "<a href=\"%1\">%2</a></h4>"
+
              "<small>";
   bool haveKeywords = url.Keywords().count() > 0;
   if (haveKeywords) {
     html_str.append(QObject::tr("<span style=\"font-style:italic\">"
                                 "Keywords: %5 </span><br>"));
   }
+  html_str.append(QObject::tr("Arado-Flashmark: "));
   html_str.append (
-             "<span style=\""
+             "<span style=\"font-size:small;"
              " font-weight:normal\">%3 &nbsp;</span>");
+
   QString aicon (QString("<img src=\"qrc:/arado-logo-colo-128.png\""
-                   " height=%1px weight=%2px >"
-                   " &nbsp;Flashmark:&nbsp;")
+                   " height=%1px weight=%2px >")
                    .arg (iconHeight).arg(iconWidth));
- // html_str.append(QObject::tr("Arado-Flashmark:"));
   html_str.append (aicon);
-  html_str.append(" %4<br>\n"
+
+  html_str.append(" %4\n"
+                  "[ <a href=\"http://translate.google.com/translate?hl=de&sl=en&u=%1\">Translate this page</a> ]\n"
+                  "<br>"
+
+                   "<div class=\"url\">"
                   "%1</small>\n"
+                   "</div>"
+
                   "</div>\n");
   if (haveKeywords) {
     return html_str.arg(url.Url().toString())
                  .arg(url.Description())
-                 .arg(Timestamp())
                  .arg(Flashmark())
+                 .arg(Timestamp())
                  .arg(Keywords());
   } else {
     return html_str.arg(url.Url().toString())
                  .arg(url.Description())
-                 .arg(Timestamp())
-                 .arg(Flashmark());
+                 .arg(Flashmark())
+                 .arg(Timestamp());
   }
 }
 
 QString
 UrlDisplayViewItem::Timestamp()
 {
-  return QDateTime::fromTime_t(url.Timestamp()).toString();
+  return QDateTime::fromTime_t(url.Timestamp()).toString(Qt::ISODate);
 }
 
 QString
