8fe044a69721361a03b1d8c03b1c80479bcc5a61
Bernd Stramm <berndhs@users.noreply.github.com>
2011-01-19 03:57:27 +0000

    bump version

--- src/version.cpp
+++ src/version.cpp
@@ -28,7 +28,7 @@
  
 namespace deliberate {
 
-  QString ProgramVersion::MyVersionNumber ("0.1.11");
+  QString ProgramVersion::MyVersionNumber ("0.1.12");
 
   QString ProgramVersion::MyProgramName("Arado");
   QString ProgramVersion::copyright ("Copyright (C) 2010 Arado Team");
